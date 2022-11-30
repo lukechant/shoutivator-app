60305dd9160fc0251f1701736a86465e1f811441
luke-chant <luke.chant@autotrader.co.uk>
2022-11-30 20:53:50 +0000

    add some nonsense

--- src/app/slogans.container/slogans-container.component.ts
+++ src/app/slogans.container/slogans-container.component.ts
@@ -3,16 +3,50 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'slogans-container',
   templateUrl: './slogans-container.component.html',
-  styleUrls: ['./slogans-container.component.css']
+  styleUrls: ['./slogans-container.component.css'],
 })
 export class SlogansContainerComponent implements OnInit {
+  slogan: string = this.sloganGenerator();
 
-  slogan = 'Chuffing Slogans!';
+  constructor() {
 
-  constructor() { }
 
-  ngOnInit(): void {
   }
 
 
+
+  ngOnInit(): void {
+    this.sloganGenerator()
+  }
+
+  chooseStart() {
+    const start: Array<string> = [
+      'Feel',
+      'Go for',
+      'Max',
+      'Change',
+      'Murder',
+      'Loosen',
+      'Interrogate',
+    ];
+    return start[Math.floor(Math.random() * start.length)];
+  }
+  chooseEnd() {
+    const end: Array<string> = [
+      'Paradigm',
+      'Chipmunk',
+      'Badger',
+      'Cheese',
+      'Burn',
+      'Gurn',
+      'Toaster',
+    ];
+    return end[Math.floor(Math.random() * end.length)];
+  }
+
+  sloganGenerator() {
+    const newStart = this.chooseStart();
+    const newEnd = this.chooseEnd();
+    return `${newStart} the ${newEnd}`;
+  }
 }
